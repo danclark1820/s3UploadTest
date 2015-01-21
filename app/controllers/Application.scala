55d1e10dcc768cd88f8b813457840d6ca0ff3f67
Dan Clark <dan.clark@quanttus.com>
2015-01-21 09:34:00 -0500

    create form

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,7 +3,20 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.data._
+import play.api.data.Forms._
+
+import java.nio.file.Files
+import java.io.File
+
 object Application extends Controller {
+
+//  case class ImageFile(name: String)
+
+//  val imageForm = Form(
+//    mapping(
+//      "file" -> nonEmptyText
+//    )(ImageFile.apply)(Iamgefile.unapply)
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
